8ed0fd2c779d82591924e094c7ff8dac1ecd9d6a
Jeff Liu <jl2964@cornell.edu>
2017-12-30 09:50:15 -0500

    sql updates

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,6 +5,7 @@
 create table vendor (
   id                        bigint not null,
   name                      varchar(255),
+  url                       varchar(255),
   constraint pk_vendor primary key (id))
 ;
 
